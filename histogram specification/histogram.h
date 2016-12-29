7308cf32f224b5741f94fd756ffeb6de321b2e2c
alexkapp <aekwdf@mail.umsl.edu>
2016-12-28 20:56:25 -0600

    Update histogram.h

--- histogram specification/histogram.h
+++ histogram specification/histogram.h
@@ -1,8 +1,7 @@
+/* Histogram Header File */
+
 #include <opencv2/opencv.hpp>
-//#ifndef HISTOGRAM_H
-//#define HISTOGRAM_H
 void equalize(cv::Mat&); 
 void equalize(cv::Mat&, cv::Mat&); 
 int * getCDF(cv::Mat);
 int * getCDF(cv::Mat, int);
-//#endif
